6c061e222ebd3f2c74fdb1fe8d6686274055482b
RAPiD <pawel.stolarski7@gmail.com>
2020-06-29 10:57:15 +0200

    Remove old code

--- commiesort.h
+++ commiesort.h
@@ -1,26 +1,8 @@
 #ifndef COMMIESORT_COMMIESORT_H
 #define COMMIESORT_COMMIESORT_H
-#endif //COMMIESORT_COMMIESORT_H
+#endif
 
 #include <vector>
-
-/*
-namespace commiesort {
-    template <class T> std::vector<T> sort(std::vector<T> vec) {
-        unsigned int length = vec.size();
-        for (unsigned int idx = 0; idx < length; idx++) {
-            // this thing needs optimizing - change this if to length-1 in FOR LOOP
-            if (idx != length - 1) {
-                if (vec[idx] >= vec[idx + 1]) {
-                    vec.erase(vec.begin() + idx + 1);
-                    return sort(vec);
-                }
-            }
-        }
-        return vec;
-    }
-}
-*/
 
 namespace commiesort {
     template<class T>
@@ -35,4 +17,3 @@ namespace commiesort {
         return vec;
     }
 }
-
